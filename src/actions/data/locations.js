45e4e30d270be8f6ed0c598d515db112078be671
keithpops <keith.poplawski@gmail.com>
2017-02-06 18:22:52 -0500

    shortens findLocationByAddress return

--- src/actions/data/locations.js
+++ src/actions/data/locations.js
@@ -14,9 +14,8 @@ export function fetchLocations(brandibble, lat=null, lng=null, orderType='olo') 
 export function findLocationByAddress(brandibbleRef, address) {
   return dispatch => {
     const { latitude, longitude } = address;
-    return dispatch(fetchLocations(brandibbleRef, latitude, longitude)).then(res => {
-      const locations = res.records;
-      const deliverableLocation = find(locations.data || [], location => location.in_delivery_zone);
+    return dispatch(fetchLocations(brandibbleRef, latitude, longitude)).then(({records}) => {
+      const deliverableLocation = find(records || [], location => location.in_delivery_zone);
       return deliverableLocation ? deliverableLocation.location_id : null;
     });
   };

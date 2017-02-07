e1d557b002b8e87aeaeff863039a132043d244e8
keithpops <keith.poplawski@gmail.com>
2017-02-06 19:06:59 -0500

    adds limit to location function - TODO: add limit handling to brandibble.js

--- src/actions/data/locations.js
+++ src/actions/data/locations.js
@@ -1,10 +1,10 @@
 import reduxCrud from 'redux-crud';
 const { fetchStart, fetchSuccess, fetchError } = reduxCrud.actionCreatorsFor('locations');
 
-export function fetchLocations(brandibble, lat=null, lng=null, orderType='olo') {
+export function fetchLocations(brandibble, lat=null, lng=null, limit=50, orderType='olo') {
   return dispatch => {
     dispatch(fetchStart());
-    return brandibble.locations.index(lat, lng, orderType)
+    return brandibble.locations.index(lat, lng, limit, orderType)
       .then(({ data }) => dispatch(fetchSuccess(data)))
       .catch(({ errors }) => dispatch(fetchError(errors)));
   };

eea9ed6bca5210524d24843badaa75e6751d29b0
keithpops <keith.poplawski@gmail.com>
2017-02-13 13:25:47 -0500

    fixes locations action to accept query object

--- src/actions/data/locations.js
+++ src/actions/data/locations.js
@@ -1,10 +1,10 @@
 import reduxCrud from 'redux-crud';
 const { fetchStart, fetchSuccess, fetchError } = reduxCrud.actionCreatorsFor('locations');
 
-export function fetchLocations(brandibble, lat=null, lng=null, limit=50, orderType='olo') {
+export function fetchLocations(brandibble, query={}) {
   return dispatch => {
     dispatch(fetchStart());
-    return brandibble.locations.index(lat, lng, limit, orderType)
+    return brandibble.locations.index(query)
       .then(({ data }) => dispatch(fetchSuccess(data)))
       .catch(({ errors }) => dispatch(fetchError(errors)));
   };

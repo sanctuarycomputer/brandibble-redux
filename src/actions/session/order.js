0d619175713aea52602a87bc0fdf2501cfd16618
keithpops <keith.poplawski@gmail.com>
2017-03-13 18:09:11 -0400

    adds bind customer to order action

--- src/actions/session/order.js
+++ src/actions/session/order.js
@@ -8,6 +8,7 @@ export const REMOVE_LINE_ITEM = 'REMOVE_LINE_ITEM';
 export const ADD_OPTION_TO_LINE_ITEM = 'ADD_OPTION_TO_LINE_ITEM';
 export const REMOVE_OPTION_FROM_LINE_ITEM = 'REMOVE_OPTION_FROM_LINE_ITEM';
 export const SET_ORDER_LOCATION_ID = 'SET_ORDER_LOCATION_ID';
+export const BIND_CUSTOMER_TO_ORDER = 'BIND_CUSTOMER_TO_ORDER';
 
 /* Private Action Creators */
 function _resolveOrder(payload) {
@@ -56,6 +57,13 @@ function _setOrderLocationId(order, locationId) {
   };
 }
 
+function _bindCustomerToOrder(order, customer) {
+  return {
+    type: BIND_CUSTOMER_TO_ORDER,
+    payload: order.setCustomer(customer).then(order => ({ order })),
+  };
+}
+
 /* Public Functions */
 export function resolveOrder(brandibble, locationId = null, serviceType = 'delivery') {
   const { orders } = brandibble;
@@ -98,4 +106,8 @@ export function addOptionToLineItem(currentOrder, lineItem, optionGroup, optionI
 
 export function removeOptionFromLineItem(currentOrder, lineItem, optionItem) {
   return dispatch => dispatch(_removeOptionFromLineItem(...arguments));
+}
+
+export function bindCustomerToOrder(...args) {
+  return dispatch => dispatch(_bindCustomerToOrder(...args));
 }

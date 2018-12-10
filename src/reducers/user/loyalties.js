2248b318653223fc2a1a0a6e276b2dfb15dab858
Ellis Marte <elliscmarte@gmail.com>
2018-12-10 12:15:42 -0500

    reset loyalties

--- src/reducers/user/loyalties.js
+++ src/reducers/user/loyalties.js
@@ -1,5 +1,9 @@
 import { FETCH_CUSTOMER_LOYALTIES } from '../../actions/user/loyalties';
 
+import {
+  UNAUTHENTICATE_USER,
+} from '../../actions/session/user';
+
 export const initialState = {
   loyalties: [],
 };
@@ -12,6 +16,8 @@ export default (state = initialState, action) => {
         ...state,
         loyalties: payload
       };
+      case `${UNAUTHENTICATE_USER}_FULFILLED`:
+        return initialState;
     default:
       return state;
   }

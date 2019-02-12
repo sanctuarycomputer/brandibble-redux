a1092c008c74047506aaeb1385385b652e0f9be8
Sebastian <sebastian@sanctuary.computer>
2019-02-12 16:45:38 -0500

    creates mock state for selector

---
+++ tests/selectors/currentOrderMenuStatus.test.js
@@ -0,0 +1,17 @@
+/* global describe before it */
+import { expect } from 'chai';
+import { brandibble } from '../config/stubs';
+
+describe('selectors/currentOrderMenuStatus', () => {
+  /**
+   * We want to ensure we have the necessary data
+   * in Redux in order to test our selector
+   */
+
+  // Setup brandibble with location id
+  // fetch location with service type, requested at
+  // and include times flag
+  before(() => {
+    debugger;
+  });
+});

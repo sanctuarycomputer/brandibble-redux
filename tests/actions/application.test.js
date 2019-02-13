555e1eb2dce8877f929a57c977b43cb37dd10b00
Sebastian <sebastian@sanctuary.computer>
2019-02-13 15:45:22 -0500

    uncomments sen support ticket

--- tests/actions/application.test.js
+++ tests/actions/application.test.js
@@ -15,9 +15,9 @@ import { brandibble } from '../config/stubs';
 const mockStore = configureStore(reduxMiddleware);
 
 describe('actions/application', () => {
-  let store, 
-action, 
-actionsCalled;
+  let store;
+  let action;
+  let actionsCalled;
   describe('setupBrandibble', () => {
     before(() => {
       store = mockStore();
@@ -61,32 +61,32 @@ actionsCalled;
     });
   });
 
-  // describe('sendSupportTicket', () => {
-  //   before(() => {
-  //     store = mockStore();
-  //     return sendSupportTicket(brandibble, {
-  //       subject: 'help!',
-  //       body: 'i need avocado!',
-  //       email: 'dev@sanctuary.computer',
-  //     })(store.dispatch).then(() => {
-  //       actionsCalled = store.getActions();
-  //     });
-  //   });
+  describe('sendSupportTicket', () => {
+    before(() => {
+      store = mockStore();
+      return sendSupportTicket(brandibble, {
+        subject: 'help!',
+        body: 'i need avocado!',
+        email: 'dev@sanctuary.computer',
+      })(store.dispatch).then(() => {
+        actionsCalled = store.getActions();
+      });
+    });
 
-  //   it('should call at least 2 actions', () => {
-  //     expect(actionsCalled).to.have.length.of.at.least(2);
-  //   });
+    it('should call at least 2 actions', () => {
+      expect(actionsCalled).to.have.length.of.at.least(2);
+    });
 
-  //   it('should have SEND_SUPPORT_TICKET_PENDING action', () => {
-  //     action = find(actionsCalled, { type: 'SEND_SUPPORT_TICKET_PENDING' });
-  //     expect(action).to.exist;
-  //   });
+    it('should have SEND_SUPPORT_TICKET_PENDING action', () => {
+      action = find(actionsCalled, { type: 'SEND_SUPPORT_TICKET_PENDING' });
+      expect(action).to.exist;
+    });
 
-  //   it('should have SEND_SUPPORT_TICKET_FULFILLED action', () => {
-  //     action = find(actionsCalled, { type: 'SEND_SUPPORT_TICKET_FULFILLED' });
-  //     expect(action).to.exist;
-  //   });
-  // });
+    it('should have SEND_SUPPORT_TICKET_FULFILLED action', () => {
+      action = find(actionsCalled, { type: 'SEND_SUPPORT_TICKET_FULFILLED' });
+      expect(action).to.exist;
+    });
+  });
 
   describe('resetApplication', () => {
     before(() => {

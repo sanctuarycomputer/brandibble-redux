e1e137b0168116a6702cb522db69977b7ee24de4
Sebastian <sebastian@sanctuary.computer>
2019-02-13 16:42:06 -0500

    updates test description

--- tests/selectors/validOrderTimeForNow.test.js
+++ tests/selectors/validOrderTimeForNow.test.js
@@ -9,7 +9,7 @@ import {
 import { validOrderTimeForNow } from '../../src/selectors';
 
 describe('selectors/validOrderTimeForNow', () => {
-  it('it should return an object based on the', () => {
+  it("it should return the valid first time object for the current order's location payload", () => {
     const testValidOrderTimeForNowWithOloOrderStub = validOrderTimeForNow(
       brandibbleStateForOloOrderStub,
     );

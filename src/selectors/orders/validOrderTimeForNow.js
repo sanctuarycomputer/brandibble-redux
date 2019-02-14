10358df62553e12873a3c2f1f20f13b40fcd289b
Sebastian <sebastian@sanctuary.computer>
2019-02-14 12:26:32 -0500

    uses getter in case location id is undefined

--- src/selectors/orders/validOrderTimeForNow.js
+++ src/selectors/orders/validOrderTimeForNow.js
@@ -12,7 +12,10 @@ export const validOrderTimeForNow = createSelector(
   state => get(state, 'session.order.orderData.service_type'),
   state => get(state, 'data.locations.locationsById'),
   (locationIdForCurrentOrder, serviceTypeForCurrentOrder, allLocationsById) => {
-    const locationForCurrentOrder = allLocationsById[locationIdForCurrentOrder];
+    const locationForCurrentOrder = get(
+      allLocationsById,
+      `${locationIdForCurrentOrder}`,
+    );
     return get(
       locationForCurrentOrder,
       `first_times.${serviceTypeForCurrentOrder}`,

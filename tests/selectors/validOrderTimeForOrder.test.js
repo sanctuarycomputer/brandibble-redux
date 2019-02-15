efb84c043fd509754d55104181f0635b9a42b1e9
Sebastian <sebastian@sanctuary.computer>
2019-02-15 17:44:30 -0500

    gets validTimeForOrder and returns expected format

--- tests/selectors/validOrderTimeForOrder.test.js
+++ tests/selectors/validOrderTimeForOrder.test.js
@@ -1,7 +1,6 @@
 /* global describe before it */
 import { expect } from 'chai';
 import { Settings, DateTime } from 'luxon';
-import luxonDateTimeFromRequestedAt from '../../src/utils/luxonDateTimeFromRequestedAt';
 
 import {
   brandibbleStateForCateringOrderStub,
@@ -209,4 +208,5 @@ describe('selectors/validOrderTimeForOrder', () => {
     )(requestedAtAsLuxonDateTime, todayAsLuxonDateTime);
 
     expect(testValidOrderTimeForOrder).to.equal.null;
+  });
 });

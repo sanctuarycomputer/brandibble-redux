3788cdc8155924fa2321d8f190a45679eb64456a
Sebastian <sebastian@sanctuary.computer>
2019-02-26 19:05:59 -0500

    cleanup

--- tests/selectors/menuStatusForOrder.test.js
+++ tests/selectors/menuStatusForOrder.test.js
@@ -7,6 +7,7 @@ import {
   stateForOloOrderStub,
   stateForOloOrderStubWithWantsFutureOrder,
   stateForOloOrderStubWithAsapRequestedAt,
+  stateForOloOrderStubWithValidRequestedAt,
 } from '../config/stateStubs';
 import { Timezones, MenuStatusCodes } from '../../src/utils/constants';
 
